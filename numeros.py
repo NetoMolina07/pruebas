bb51f83fd9d990030f65aceaac0a2f98db062f2b
NetoMolina07 <126990629+NetoMolina07@users.noreply.github.com>
2023-05-18 18:35:57 -0600

    Add files via upload

---
+++ numeros.py
@@ -0,0 +1,15 @@
+#pedir numeros al usuario
+numero1 = int(input("ingresa un numero (1): "))
+numero2 = int(input("ingresa un numero (2): "))
+
+#imprimir  resultados
+
+print(f'la suma es {numero1 + numero2}')
+#resta
+print(f'la resta es {numero1 - numero2}')
+#multiplicacion
+print(f'la multiplicaciom es {numero1 * numero2}')
+#division
+print(f'la division es {numero1 / numero2}')
+#modulo
+print(f'el modulo es {numero1 % numero2}')
