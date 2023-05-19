ebd52318c74c4912c494a77c219d5d6935ac27ab
NetoMolina07 <126990629+NetoMolina07@users.noreply.github.com>
2023-05-18 18:36:38 -0600

    Add files via upload

---
+++ variables.py
@@ -0,0 +1,15 @@
+nombre = " neto "
+numero =  90292030
+estatura = 1.79
+
+print("hola" + nombre + "bienvenido al mundo de la programación " )
+
+print("hola" + nombre + "tu numero es " + str(numero))
+
+print(f'hola {nombre} tu numero telefonico es: {numero}')
+
+print(f'hola {nombre} tu numero telefonico es{numero} y tu estatura es {estatura}')
+
+print(type(nombre))
+print(type(numero))
+print(type(estatura))
